161f80051498604de704c0415ba0ba053bdc2740
SabrinaRDC <hethordc+github@gmail.com>
2022-10-10 04:55:16 +0800

    fixed out of bounds clipping

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
 let balls = [];
-let gravity = 80;
 let FPS = 60;
+let gravity = 500;
 let sizeX = 1000;
 let sizeY = 500;
-let bounceEfficiency = 0.93;
+let bounceEfficiency = 1;
 function createBall(diameter, color = "000000", posX = 0, posY = 0, volX = 0, volY = 0 ) {
     let setID;
     if (balls[(balls.length - 1)] !== undefined){
@@ -25,39 +25,32 @@ function createBall(diameter, color = "000000", posX = 0, posY = 0, volX = 0, vo
         }
     )
 };
-
 function calculateBall(){
     balls.forEach(ball => {
-        ball.volocity.y += gravity/FPS*10;
+        ball.volocity.y += gravity/FPS;
         ball.position.x += ball.volocity.x/FPS;
         ball.position.y += ball.volocity.y/FPS; 
-        
-        
-        if (ball.position.x < 0 || ball.position.x + ball.diameter >= sizeX){
+        if (ball.position.x < 0){
             ball.volocity.x = ball.volocity.x * -1 * bounceEfficiency;
+            ball.position.x = 0;
         };
-        if (ball.position.y < 0 || ball.position.y + ball.diameter >= sizeY){
+        if (ball.position.x + ball.diameter >= sizeX){
+            ball.volocity.x = ball.volocity.x * -1 * bounceEfficiency;
+            ball.position.x = sizeX - ball.diameter;
+        };
+        if (ball.position.y < 0){
             ball.volocity.y = ball.volocity.y * -1 * bounceEfficiency;
+            ball.position.y = 0;
         };
-        if ((ball.position.x + ball.diameter) > sizeX) {
-            ball.position.x = sizeX - ball.diameter
+        if (ball.position.y + ball.diameter >= sizeY){
+            ball.volocity.y = ball.volocity.y * -1 * bounceEfficiency;
+            ball.position.y = sizeY - ball.diameter;
         };
-        if ((ball.position.y + ball.diameter) > sizeY) {
-            ball.position.y = sizeY - ball.diameter
-        };
-        if (ball.position.x < 0) {
-            ball.position.x = 0
-        };
-        if (ball.position.y < 0) {
-            ball.position.y = 0
-        }
     })
 };
-
 function drawBall(){
-    let HTML = "";
-    HTML += `<span style="border-color: #000000; display: block; position: absolute; top: 0px; left: 0px; width: ${sizeX}px; height: ${sizeY}px; border-style: dashed;"></span>
-    `
+    let HTML = `<span style="border-color: #000000; display: block; position: absolute; top: 0px; left: 0px; width: ${sizeX}px; height: ${sizeY}px; border-style: solid;"></span>
+    `;
     if (balls[0] !== undefined){
         balls.forEach(ball =>{
         HTML += `<span class="ball" id="${ball.id}" style="display: block; position: absolute; border-radius: 50%; background-color: #${ball.color}; width: ${ball.diameter}px; height: ${ball.diameter}px; left: ${ball.position.x}px; top: ${ball.position.y}px"></span>
@@ -65,7 +58,6 @@ function drawBall(){
     };
     document.querySelector('body').innerHTML = HTML;
 };
-
 function createRandomBall(maxVolocity, diameter, color = "000000"){
     function randomVolocity(){
         let volocity;
@@ -73,21 +65,22 @@ function createRandomBall(maxVolocity, diameter, color = "000000"){
         if (Math.round(Math.random())){ volocity *= -1};
         return volocity;
     };
-    function randomPosition(max){return max * Math.random()};
+    function randomPosition(max){
+        let pos;
+        pos = max * Math.random();
+        if ((pos + diameter) > max){pos = (max - diameter)};
+        return pos;
+    };
     createBall(diameter, color, randomPosition(sizeX), randomPosition(sizeY), randomVolocity(),randomVolocity());
 };
-
-// createBall(100, "FF00FF", 100, 100, 100, -100);
-createRandomBall(Math.random() * 100, Math.random()* 100);
-createRandomBall(Math.random() * 100, Math.random()* 100);
-createRandomBall(Math.random() * 100, Math.random()* 100);
-createRandomBall(Math.random() * 100, Math.random()* 100);
-createRandomBall(Math.random() * 100, Math.random()* 100);
-
-
-
 setInterval(()=>{
     calculateBall();
     drawBall();
-    console.log(balls[0].position.x, balls[0].position.y,balls[0].volocity.x, balls[0].volocity.y)
 }, (1000/FPS))
+
+createRandomBall(200, 50);
+createRandomBall(200, 50);
+createRandomBall(200, 50);
+createRandomBall(200, 50);
+createRandomBall(200, 50);
+
